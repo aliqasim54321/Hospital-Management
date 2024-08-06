a7e79793b7d66e514a4b93d30b8d23faf0f1efaa
lyeap <lyeapaul@gmail.com>
2024-08-06 18:45:46 -0400

    corrected update method in views

--- HospitalManagement/Controllers/OrderDataController.cs
+++ HospitalManagement/Controllers/OrderDataController.cs
@@ -15,7 +15,16 @@ namespace HospitalManagement.Controllers
     public class OrderDataController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
+        /// <summary>
+        /// Returns all orders in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: all orders in the database, including their associated patient and order  price.
+        /// </returns>
+        /// <example>
+        /// GET: api/OrderData/ListOrder
+        /// </example>
         // GET: api/OrderData/ListOrders
         [HttpGet]
         public IEnumerable<OrderDto> ListOrders()
@@ -32,7 +41,19 @@ namespace HospitalManagement.Controllers
             }));
             return OrderDtos;
         }
-
+        /// <summary>
+        /// Returns an order in the system.
+        /// </summary>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// CONTENT: An order in the system matching up to the order ID primary key
+        /// or
+        /// HEADER: 404 (NOT FOUND)
+        /// </returns>
+        /// <param name="id">The primary key of the orderl</param>
+        /// <example>
+        /// GET: api/OrderData/FindOrder/5
+        /// </example>
         // GET: api/OrderData/FindOrder/5
         [ResponseType(typeof(OrderDto))]
         [HttpGet]
@@ -55,6 +76,22 @@ namespace HospitalManagement.Controllers
             return Ok(OrderDto);
         }
 
+        /// <summary>
+        /// Updates a particular order in the system with POST Data input
+        /// </summary>
+        /// <param name="id">Represents the Order ID primary key</param>
+        /// <param name="order">JSON FORM DATA of an order</param>
+        /// <returns>
+        /// HEADER: 204 (Success, No Content Response)
+        /// or
+        /// HEADER: 400 (Bad Request)
+        /// or
+        /// HEADER: 404 (Not Found)
+        /// </returns>
+        /// <example>
+        /// POST: api/OrderData/UpdateOrder/5
+        /// FORM DATA: Order JSON Object
+        /// </example>
         // POST: api/OrderData/UpdateOrder/5
         [ResponseType(typeof(void))]
         [HttpPost]
@@ -97,6 +134,20 @@ namespace HospitalManagement.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
+        /// <summary>
+        /// Adds an order to the system
+        /// </summary>
+        /// <param name="order">JSON FORM DATA of an order</param>
+        /// <returns>
+        /// HEADER: 201 (Created)
+        /// CONTENT: Order ID, Order Data
+        /// or
+        /// HEADER: 400 (Bad Request)
+        /// </returns>
+        /// <example>
+        /// POST: api/OrderData/AddOrder
+        /// FORM DATA: Order JSON Object
+        /// </example>
         // POST: api/OrderData/AddOrder
         [ResponseType(typeof(Order))]
         [HttpPost]
@@ -112,7 +163,19 @@ namespace HospitalManagement.Controllers
 
             return CreatedAtRoute("DefaultApi", new { id = order.Order_id }, order);
         }
-
+        /// <summary>
+        /// Deletes an order from the system by it's ID.
+        /// </summary>
+        /// <param name="id">The primary key of the order</param>
+        /// <returns>
+        /// HEADER: 200 (OK)
+        /// or
+        /// HEADER: 404 (NOT FOUND)
+        /// </returns>
+        /// <example>
+        /// POST: api/OrderData/DeleteOrder/5
+        /// FORM DATA: (empty)
+        /// </example>
         // POST: api/OrderData/DeleteOrder/5
         [ResponseType(typeof(Order))]
         [HttpPost]
