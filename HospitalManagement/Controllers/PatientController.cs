6025564df9f804195fc59fceab1834cc0c8909fc
aliqasimkhundmiri10@gmail.com <rehaan@Guest>
2024-08-07 02:53:51 -0400

    Added listin Views

---
+++ HospitalManagement/Controllers/PatientController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using HospitalManagement.Models;
+
+
+namespace HospitalManagement.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult List()
+        {
+            HttpClient client = new HttpClient() { };
+            string url = "https://localhost:44361/api/patientdata/listpatients";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Debug.WriteLine("Number of Patient arrived");
+            Debug.WriteLine("The response code is");
+           Debug.WriteLine(response.StatusCode);
+
+            IEnumerable<PatientDto> patients = response.Content.ReadAsAsync<IEnumerable<PatientDto>>().Result;
+             
+            return View(patients);
+        }
+
+        // GET: Patient/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Patient/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patient/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patient/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Patient/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Patient/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Patient/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
